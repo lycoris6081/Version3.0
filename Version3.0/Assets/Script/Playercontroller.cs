3f6f0e5ac1e2561c02361195c5dc878ceb63c2e0
HAN <hanhan5556888@gmail.com>
2023-09-04 21:57:40 +0800

    導入貓貓圖片 (待機&攻擊)；更新滑鼠控制移動；新增Animator&Animation檔案 ；尚未導入攻擊程式邏輯

---
+++ Version3.0/Assets/Script/Playercontroller.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+
+
+    void Update()
+    {
+        // 獲取滑鼠當前的屏幕位置
+        Vector3 mousePosition = Input.mousePosition;
+
+        // 將滑鼠位置從屏幕坐標轉換為世界坐標
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, transform.position.z));
+
+        // 計算物體到滑鼠位置的方向向量
+        Vector3 moveDirection = worldMousePosition - transform.position;
+
+        // 鎖住Z軸，使物體保持在2D平面上
+        moveDirection.z = 0;
+
+        // 正規化方向向量，以確保速度一致
+        moveDirection.Normalize();
+
+        // 設置物體的位置
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+
+       
+    }
+
+    
+}
+
