991652e8035729748a6edd1eaca18177f04c0dac
DESKTOP-SPD4CPI\ubin <gos920224@gmail.com>
2023-09-26 01:54:30 +0800

    ui介面升級。現在焦點在按鈕上可以看到貓屁屁了

---
+++ Version3.0/Assets/Script(YB)/BIC.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BIC : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Image buttonImage; 
+    public Sprite imageOne; 
+    public Sprite imageTwo; 
+
+    private void Start()
+    {
+        buttonImage.sprite = imageOne; 
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonImage.sprite = imageTwo; 
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonImage.sprite = imageOne; 
+    }
+}
+
