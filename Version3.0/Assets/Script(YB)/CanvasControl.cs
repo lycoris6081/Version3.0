b33f1bb7b2588739e46f849c8b3fcd6902b046af
DESKTOP-SPD4CPI\ubin <gos920224@gmail.com>
2023-09-27 02:49:57 +0800

    音量條問題優化完成，setup開啟方式由進入sences優化為跳出canva

---
+++ Version3.0/Assets/Script(YB)/CanvasControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasControl : MonoBehaviour
+{
+    public GameObject canvasToToggle; // 引用第二个Canvas
+
+    void Start()
+    {
+        canvasToToggle.SetActive(false); // 初始时将第二个Canvas设置为不可见
+    }
+
+    public void ToggleCanvas()
+    {
+        canvasToToggle.SetActive(!canvasToToggle.activeSelf); // 切换第二个Canvas的可见性
+    }
+}
+
---
+++ Version3.0/Assets/Script(YB)/CanvasControl2.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasControl2 : MonoBehaviour
+{
+    public GameObject canvasToToggle; // 引用需要关闭的Canvas
+
+    public void CloseCanvas()
+    {
+        canvasToToggle.SetActive(false); // 关闭Canvas，将其设置为不可见
+    }
+}
+
