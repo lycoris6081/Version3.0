a2c555a99622869e85e76cce647c313c92bc7837
DESKTOP-SPD4CPI\ubin <gos920224@gmail.com>
2023-10-14 00:20:14 +0800

    關卡選擇頁面建成

---
+++ Version3.0/Assets/Script(YB)/ChangeImage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeImage : MonoBehaviour
+{
+    public Image imageToChange; // 你需要在Unity编辑器中将你的Image组件分配给这个变量
+    public Image newImage;      // 新的Image组件
+
+    public void ChangeImageOnClick()
+    {
+        if (imageToChange != null && newImage != null)
+        {
+            imageToChange.sprite = newImage.sprite;
+        }
+    }
+}
+
