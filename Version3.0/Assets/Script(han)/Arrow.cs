556633b34e415d8dfafbc37e1cc3567eb3417f37
M307-20\shu <hanhan5556888gmail.com>
2023-10-31 15:07:12 +0800

    更新箱子優化

---
+++ Version3.0/Assets/Script(han)/Arrow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public Transform target; // 箭头指向的目标，即箱子
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            // 使箭头指向目标位置
+            transform.LookAt(target);
+        }
+    }
+}
--- Version3.0/Assets/Script(han)/Boxcontroller.cs
+++ Version3.0/Assets/Script(han)/Boxcontroller.cs
@@ -6,12 +6,12 @@ public class Boxcontroller : MonoBehaviour
 {
 
     public GameObject BoxPrefab;
-
+    public GameObject ArrowIndicatorPrefab; // 箭头预制体
     public int NeedsSoul = 10;//需要的靈魂數量
 
     private bool IsBoxSpawned = false;
     private int currentSoulCount = 0; // 当前的灵魂获取量
-    
+    private GameObject Arrow; // 用于存储箭头的引用
 
     public void CheckSoulcount(int soulCount)
     {
@@ -26,15 +26,24 @@ public class Boxcontroller : MonoBehaviour
 
     void SpawnBox()
     {
-        Instantiate(BoxPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        // 随机生成箱子的位置
+        float randomX = Random.Range(-10f, 10f); // 你可以根据需要调整范围
+        float randomY = Random.Range(-10f, 10f); // 你可以根据需要调整范围
+        Vector3 spawnPosition = new Vector3(randomX, randomY, 0f);
+
+        GameObject box = Instantiate(BoxPrefab, spawnPosition, Quaternion.identity); 
         IsBoxSpawned = true;
-       
+
+        // 实例化箭头并将其指向箱子
+        Arrow = Instantiate(ArrowIndicatorPrefab, Vector3.zero, Quaternion.identity);
+        Arrow.GetComponent<Arrow>().target = box.transform;
     }
 
     public void DestroyBox()
     {
         
         Destroy(GameObject.FindGameObjectWithTag("Box"));
+        Destroy(Arrow); // 销毁箭头
         IsBoxSpawned = false;
     }
 
