30d43352edbd11456df6ed0c23ee7356e1dc8d95
DESKTOP-SPD4CPI\ubin <gos920224@gmail.com>
2023-11-01 21:14:20 +0800

    新增敵人計數程式

---
+++ Version3.0/Assets/Script(YB)/GM.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour
+{
+    public Text enemyCountText;
+    private int enemyCount = 0;
+
+    // 当擊敗擁有"enemy"標籤的敵人時調用此方法
+    public void EnemyDefeated()
+    {
+        enemyCount++;
+        UpdateUI();
+    }
+
+    // 更新UI文本显示
+    private void UpdateUI()
+    {
+        enemyCountText.text = "擊敗敵人數量: " + enemyCount.ToString();
+    }
+}
+
