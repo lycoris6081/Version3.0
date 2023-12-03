981069e0fa6a82aba7573cc363cc9da4ac455f54
DESKTOP-SPD4CPI\ubin <gos920224@gmail.com>
2023-12-04 05:06:57 +0800

    修改1-1-1-4右側魚的圖層，並將選單頁面修改程式碼改成主人家頁面為預設

--- Version3.0/Assets/Script(YB)/ButtonController.cs
+++ Version3.0/Assets/Script(YB)/ButtonController.cs
@@ -9,50 +9,77 @@ public class ButtonController : MonoBehaviour
     public Image image3;
     public Image image4;
 
+    [Header("預設顯示的圖片")]
+    [SerializeField]
+    private int defaultImageIndex = 1; // Default to the first image
+
     // Start is called before the first frame update
     void Start()
     {
-        // 初始設置，只顯示第一張圖片
-        image1.gameObject.SetActive(true);
+        SetDefaultImage();
+    }
+
+    // Set the default image based on the specified index
+    void SetDefaultImage()
+    {
+        image1.gameObject.SetActive(false);
         image2.gameObject.SetActive(false);
         image3.gameObject.SetActive(false);
         image4.gameObject.SetActive(false);
+
+        switch (defaultImageIndex)
+        {
+            case 1:
+                image1.gameObject.SetActive(true);
+                break;
+            case 2:
+                image2.gameObject.SetActive(true);
+                break;
+            case 3:
+                image3.gameObject.SetActive(true);
+                break;
+            case 4:
+                image4.gameObject.SetActive(true);
+                break;
+            default:
+                Debug.LogWarning("Invalid defaultImageIndex. Defaulting to the first image.");
+                image1.gameObject.SetActive(true);
+                break;
+        }
     }
 
     // 選擇顯示第一張圖片
     public void OnButton1Click()
     {
-        image1.gameObject.SetActive(true);
-        image2.gameObject.SetActive(false);
-        image3.gameObject.SetActive(false);
-        image4.gameObject.SetActive(false);
+        SetImagesActiveState(true, false, false, false);
     }
 
     // 選擇顯示第二張圖片
     public void OnButton2Click()
     {
-        image1.gameObject.SetActive(false);
-        image2.gameObject.SetActive(true);
-        image3.gameObject.SetActive(false);
-        image4.gameObject.SetActive(false);
+        SetImagesActiveState(false, true, false, false);
     }
 
     // 選擇顯示第三張圖片
     public void OnButton3Click()
     {
-        image1.gameObject.SetActive(false);
-        image2.gameObject.SetActive(false);
-        image3.gameObject.SetActive(true);
-        image4.gameObject.SetActive(false);
+        SetImagesActiveState(false, false, true, false);
     }
 
     // 選擇顯示第四張圖片
     public void OnButton4Click()
     {
-        image1.gameObject.SetActive(false);
-        image2.gameObject.SetActive(false);
-        image3.gameObject.SetActive(false);
-        image4.gameObject.SetActive(true);
+        SetImagesActiveState(false, false, false, true);
+    }
+
+    // Helper method to set the active state of images
+    void SetImagesActiveState(bool active1, bool active2, bool active3, bool active4)
+    {
+        image1.gameObject.SetActive(active1);
+        image2.gameObject.SetActive(active2);
+        image3.gameObject.SetActive(active3);
+        image4.gameObject.SetActive(active4);
     }
 }
+
 
