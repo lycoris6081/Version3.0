98a9b1736d55cc6439a32f1f99d754470bd4f63e
DESKTOP-SPD4CPI\ubin <gos920224@gmail.com>
2023-12-04 06:05:13 +0800

    修改程式碼讓時間能順利暫停

--- Version3.0/Assets/Script(han)/GameClear.cs
+++ Version3.0/Assets/Script(han)/GameClear.cs
@@ -11,12 +11,19 @@ public class GameClear : MonoBehaviour
     {
         if (other.gameObject.tag == "CATCAT")
         {
-            
             if (gameClearCanvas != null)
             {
                 gameClearCanvas.SetActive(true);
+                PauseGame();
             }
         }
     }
+
+    private void PauseGame()
+    {
+        // Pause the game by setting the time scale to 0
+        Time.timeScale = 0f;
+    }
 }
+
 
