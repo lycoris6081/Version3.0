79d7c3859a0e5925a6d8bda305b3a44900f647f0
izumiayaa <A110082018@mail.shu.edu.tw>
2023-12-04 21:06:22 +0800

    加上了1-1剩下的音效+修改卡片1文本

--- Version3.0/Assets/Script(YB)/AudioManager.cs
+++ Version3.0/Assets/Script(YB)/AudioManager.cs
@@ -5,10 +5,19 @@ using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-
+    [Header("按鈕音效")]
     public AudioClip ButtonASound;
     public AudioClip ButtonBSound;
     public AudioClip CatSound;
+    
+    [Header("特效音效")]
+    public AudioClip Ability_Heal;
+    public AudioClip Ability_twohit;
+    public AudioClip Ability_DrawAAgain;
+    public AudioClip Ability_CantHurt;
+    public AudioClip Ability_bullettime;
+    public AudioClip Ability_Boom;
+
 
     public AudioMixer audioMixer;    // 進行控制的Mixer變量
     AudioSource audioSource;
@@ -49,5 +58,33 @@ public class AudioManager : MonoBehaviour
     public void PlayCatSound()
     {
         audioSource.PlayOneShot(CatSound);
+    }
+
+    public void PlayAbility1Sound()
+    {
+        audioSource.PlayOneShot(Ability_Heal);
+    }
+
+    public void PlayAbility2Sound()
+    {
+        audioSource.PlayOneShot(Ability_Boom);
+    }
+    public void PlayAbility4Sound()
+    {
+        audioSource.PlayOneShot(Ability_twohit);
+    }
+
+    public void PlayAbility5Sound()
+    {
+        audioSource.PlayOneShot(Ability_CantHurt);
+    }
+    public void PlayAbility6Sound()
+    {
+        audioSource.PlayOneShot(Ability_bullettime);
+    }
+
+    public void PlayAbility7Sound()
+    {
+        audioSource.PlayOneShot(Ability_DrawAAgain);
     }
 }
